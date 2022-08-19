1d669f8123e20a945e8c08c7d268abcb13d55813
casentino <casentinok@naver.com>
2022-08-20 05:36:55 +0900

    누적 로직 함수로 추출

--- src/chapter01/1.1/refactoring/statement.js
+++ src/chapter01/1.1/refactoring/statement.js
@@ -41,13 +41,19 @@ function statement(invoice, plays) {
   }
   function usd(aNumber) { 
     return  new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+  function totalVolumeCredits() { 
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits
   }
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;  
 
   for (let perf of invoice.performances) {    
@@ -55,11 +61,8 @@ function statement(invoice, plays) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
-  for (let perf of invoice.performances) { 
-    volumeCredits += volumeCreditsFor(perf)
-  }
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
 }
