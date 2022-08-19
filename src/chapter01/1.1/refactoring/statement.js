2f2689590621bd243b137989dd46ef072359af92
casentino <casentinok@naver.com>
2022-08-20 05:01:40 +0900

    함수 사용되는 변수명 수정

--- src/chapter01/1.1/refactoring/statement.js
+++ src/chapter01/1.1/refactoring/statement.js
@@ -30,14 +30,14 @@ function statement(invoice, plays) {
     }
     return result;
   }
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(aPerformance).type) {
+      result += Math.floor(aPerformance.audience / 5);
     }
-    return volumeCredits;
+    return result;
   }
   let totalAmount = 0;
   let volumeCredits = 0;
