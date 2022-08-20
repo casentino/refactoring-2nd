f53b9073a7317b0be65e8c3c08023e05ea310327
casentino <casentinok@naver.com>
2022-08-20 20:39:38 +0900

    누적 계산 반복문 파이프라인으로 바꾸기 (reduce)

--- src/chapter01/1.1/refactoring/statement.js
+++ src/chapter01/1.1/refactoring/statement.js
@@ -48,21 +48,12 @@ function statement(invoice, plays) {
     }
     return result;
   }
-  function totalVolumeCredits(data) {
-     let result = 0;
-     for (let perf of data.performances) {
-       result += perf.volumeCredits;
-     }
-     return result;
-   }
-  function totalAmount(data) {
-     let result = 0;
-     for (let perf of data.performances) {
-       result += perf.amount;
-     }
-     return result;
-   }
-  
+  function totalAmount(data) {    
+    return data.performances.reduce((total, p) => total + p.amount, 0);
+  }
+  function totalVolumeCredits(data) {    
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }  
   return renderPlainText(statementData, plays);
 }
 
