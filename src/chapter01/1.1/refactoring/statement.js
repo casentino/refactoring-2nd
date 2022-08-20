8eeadbc86c9fdbce0322720f84d03a1b200b7d02
casentino <casentinok@naver.com>
2022-08-20 20:24:43 +0900

    데이터 처리 관련 부분 enrichPerformance  로 추출

--- src/chapter01/1.1/refactoring/statement.js
+++ src/chapter01/1.1/refactoring/statement.js
@@ -2,21 +2,25 @@ const invoices = require("../invoices.json");
 const plays = require("../plays.json");
 
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
-}
-
-function renderPlainText(invoice, plays) {
-  // aPerformance를 통해 play 를 얻으므로
-  // amountFor 내부에서 다시 계산도 가능하다
+  const statementData = {};  
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  
+  function enrichPerformance(aPerformance) { 
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    return result;
+  }
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-  // perf -> aPerformance
-  // 매개변수가 역할이 뚜렷하지 않은 경우 부정관사(a/an)를 붙임
+  return renderPlainText(statementData, invoice, plays);
+}
+
+function renderPlainText(data, plays) {
   function amountFor(aPerformance) {
-    // statement 함수 내부에서 선언된 amountFor에서 playFor 를 직접 호출가능해서 play 매개변수 재거
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy": // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -31,7 +35,7 @@ function renderPlainText(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
     return result;
   }
@@ -39,7 +43,7 @@ function renderPlainText(invoice, plays) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(aPerformance).type) {
+    if ("comedy" === aPerformance.play.type) {
       result += Math.floor(aPerformance.audience / 5);
     }
     return result;
@@ -53,24 +57,24 @@ function renderPlainText(invoice, plays) {
   }
   function totalVolumeCredits() { 
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
   }
   function totalAmount() { 
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
        result += amountFor(perf);
     }
     return result;
   }
   
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`;  
+  let result = `청구 내역 (고객명: ${data.customer})\n`;  
 
-  for (let perf of invoice.performances) {    
+  for (let perf of data.performances) {    
     //청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;  
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;  
   }
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
